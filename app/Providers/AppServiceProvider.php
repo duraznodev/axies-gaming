68a03f5899b476c1745c0842c6136f1c0e330249
duraznodev <romanrizodev@gmail.com>
2023-06-14 11:40:10 -0600

    add model shouldBeStrict if not production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict(!$this->app->isProduction());
         Relation::enforceMorphMap([
             'item' => 'App\Models\Item',
             'collection' => 'App\Models\Collection',
